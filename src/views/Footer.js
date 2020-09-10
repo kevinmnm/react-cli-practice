1c87e19c8b3c4fadc39a9cddd6e08aa64f7f92a9
Kevin Song <kevinmnm2010@gmail.com>
2020-09-10 17:06:26 -0400

    practicing class defined component, footer

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -2,10 +2,53 @@ import React from 'react';
 import './Footer.css';
 
 class Footer extends React.Component {
+   state = {
+      title: 'FOOTER',
+      random: {
+         number: null,
+         text: null,
+         boolean: null
+      }
+   }
+   
+   random_number = () => {
+
+   }
+
+
    render() {
       return (
          <div className='footer'>
-            This is footer
+            <div className='footer-title'><u>{this.state.title}</u></div>
+
+            <div className='flex-wrap'>
+
+               <div className='random-number'>
+                  <div>Random Number</div>
+                  <div className='number random-value'>
+                     { this.state.random.number }
+                  </div>
+                  <button>Click</button>
+               </div>
+            
+               <div className='random-text'>
+                  <div>Random Text</div>
+                  <div className='text random-value'>
+                     { this.state.random.text }
+                  </div>
+                  <button>Click</button>
+               </div>
+
+               <div className='random-boolean'>
+                  <div>Random Boolean</div>
+                  <div className='boolean random-value'>
+                     { this.state.random.boolean }
+                  </div>
+                  <button>Click</button>
+               </div>
+            
+            </div>
+
          </div>
       )
    };
